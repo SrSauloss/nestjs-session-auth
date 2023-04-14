72b5760d4dafb95f9924dfda9db4d42555e53d79
Helio Endrio <helio.rodrigues>
2023-04-14 11:46:10 -0400

    Login/Logout/SignUp/Protected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:admin123@cluster0.pr9zf.mongodb.net/session-auth?retryWrites=true&w=majority',
+      'mongodb+srv://admin:admin123@cluster0.pr9zf.mongodb.net/session-auth?retryWrites=true&w=majority'
     ),
     UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
-
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { UsersController } from './users.controller'
+import { UserSchema } from './users.model'
+import { UsersService } from './users.service'
 @Module({
-  providers: [UsersService],
+  imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
   controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService]
 })
 export class UsersModule {}
